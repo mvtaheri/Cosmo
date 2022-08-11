2b57ed2043f2e4a800f7f9f5720dc2bac89627de
mohammad taheri <taheri@netbina.com>
2022-08-11 12:19:07 +0430

    fix bug on api call

--- index.php
+++ index.php
@@ -19,8 +19,6 @@ switch ($base_uri) {
     case '/convert':
         require __DIR__.'/src/convert.php';
         break;
-   case '/getData':
-          require __DIR__ .'/src/convert.php';
     default:
         http_response_code(404);
           echo "404";
